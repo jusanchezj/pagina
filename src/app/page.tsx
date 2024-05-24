4de24f87599de6d458e31b85c9f015a14b9f735d
Juan José Sánchez Jiménez <jusanchezj@192.168.1.6>
2024-05-23 20:18:14 -0500

    Corrige pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <title>Restaurant Gourmet & Parrilla</title>
       </Head>
 
-      <main className="min-h-screen relative overflow-hidden w-full">
+      <main className="min-h-screen relative overflow-hidden w-full bg-black">
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50 z-10"
           style={{
@@ -20,12 +20,16 @@ export default function Home() {
         />
         <section className="relative inset-0 min-h-screen w-full bg-transparent flex flex-col items-center p-8 md:p-12 z-[20]">
           <Link href="/" className="font-semibold cursor-pointer">
-            <img src="/logo sin fondo.PNG" alt="Descripción de la imagen" className="block max-w-[400px] h-[auto] mt-[-40px] ml-[3px]" />
+            <img
+              src="/logo sin fondo.PNG"
+              alt="Descripción de la imagen"
+              className="block h-auto max-w-[340px] sm:max-w-[400px] mt-[-40px] ml-[3px]"
+            />
           </Link>
 
           <div className="flex-grow flex items-center justify-center w-full">
             <div className="flex flex-col items-center text-center">
-              <h2 className="text-[#FACE8D] font-dancing text-2xl sm:text-[30px] md:text-[50px] lg:text-[70px] leading-none">
+              <h2 className="text-[#FACE8D] font-dancing text-[46px] sm:text-[50px] md:text-[70px] leading-none">
                 Servimos con calidad
               </h2>
               <h1 className="font-medium text-5xl sm:text-6xl md:text-[50px] lg:text-[70px] leading-none">
@@ -37,7 +41,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="w-full flex-col justify-center max-w-[650px]">
+          <div className="w-full flex-col justify-center max-w-[650px] mb-[30px]"> {/* Ajuste para mover el botón hacia arriba */}
             <NavigationFooter />
           </div>
         </section>
@@ -45,5 +49,3 @@ export default function Home() {
     </>
   );
 }
-
-
