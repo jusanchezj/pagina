71051fae4c57bc1d3f95d60b00bc428228543a6f
Juan José Sánchez Jiménez <jusanchezj@192.168.1.6>
2024-05-26 15:22:48 -0500

    Corrige 30px mt

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -165,13 +165,7 @@ export default function Classes() {
         .navigation-button.prev {
           left: 10px;
         }
-
-        .sticky-container {
-          position: sticky;
-          top: 0;
-          background-color: rgba(255, 255, 255, 0.9); /* Color de fondo opcional */
-          z-index: 1000;
-        }
+        
 
         @media (max-width: 540px) {
           .text-[35px] {
@@ -185,7 +179,7 @@ export default function Classes() {
         <Swiper
           loop
           autoplay={{
-            delay: 1000,
+            delay: 100000,
           }}
           slidesPerView={row}
           spaceBetween={0}
@@ -259,7 +253,7 @@ export default function Classes() {
               src="/logo sin fondo.PNG"
               alt="Descripción de la imagen"
               ref={imageRef}
-              className="block h-auto max-w-[340px] sm:max-w-[400px] mt-[0px] ml-[-15px]"
+              className="block h-auto max-w-[340px] sm:max-w-[400px] mt-[30px] ml-[-15px]"
             />
           </Link>
           <div style={{ width: imageWidth }} className="flex justify-center">
