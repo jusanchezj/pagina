b7659a535d16866542873633d9fcddc1844ad627
Juan José Sánchez Jiménez <jusanchezj@192.168.1.6>
2024-05-26 16:00:57 -0500

    Corrige 45px sm  mt

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -253,7 +253,7 @@ export default function Classes() {
               src="/logo sin fondo.PNG"
               alt="Descripción de la imagen"
               ref={imageRef}
-              className="block h-auto max-w-[340px] sm:max-w-[400px] mt-[0px] ml-[-15px]"
+              className="block h-auto max-w-[340px] sm:max-w-[400px] sm:mb-[45px] ml-[-15px]"
             />
           </Link>
           <div style={{ width: imageWidth }} className="flex justify-center">
